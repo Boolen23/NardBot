a25820de6212fc8fcdc5d9093370d10e81d89f01
dis3122 <dis3122@gmail.com>
2021-07-08 23:22:04 +0300

    3

--- NardBot/Program.cs
+++ NardBot/Program.cs
@@ -17,7 +17,6 @@ namespace NardBot
         {
             Console.Clear();
             foreach (Cell cl in game.Cells)
-                if(cl.FourthNumber < 2)
                 DrawCell(cl);
         }
         static void DrawCell(Cell c)
@@ -25,23 +24,41 @@ namespace NardBot
             bool IsSecondLine = c.FourthNumber > 1;
             var t = c.FourthNumber * 24 + c.CellNumber * 4;
             //int EndPos = 48 - c.FourthNumber * 24 - c.CellNumber * 4;
-            int EndPos = c.FourthNumber == 0 ? 58 - c.CellNumber * 4 :
-                            c.FourthNumber == 1 ? 28 - c.CellNumber * 4 : 0;
+            int Pos = c.FourthNumber == 0 ? 58 - c.CellNumber * 4 :
+                            c.FourthNumber == 1 ? 28 - c.CellNumber * 4 :
+                            c.FourthNumber == 2 ? 4 + c.CellNumber * 4 :
+                            c.FourthNumber == 3 ? 34 + c.CellNumber * 4 : 0;
             string ChipCountText = c.ChipCount < 10 ? $"0{c.ChipCount}" : c.ChipCount.ToString();
-            DrawCellFrom(c, IsSecondLine, EndPos, ChipCountText);
+            DrawCellFrom(c, IsSecondLine, Pos, ChipCountText);
         }
-        static void DrawCellFrom(Cell c, bool IsSecondLine, int EndPos, string ChipCountText)
+        static void DrawCellFrom(Cell c, bool IsSecondLine, int Pos, string ChipCountText)
         {
-            Console.SetCursorPosition(EndPos, IsSecondLine ? 2 : 0);
-            Console.Write("|");
-            Console.SetCursorPosition(EndPos - 1 < 0 ? 0 : EndPos - 1, IsSecondLine ? 2 : 0);
-            if (c.Identity == CellIIdentity.Free) Console.Write("н");
-            if (c.Identity == CellIIdentity.White) Console.Write("б");
-            if (c.Identity == CellIIdentity.Black) Console.Write("ч");
-            Console.SetCursorPosition(EndPos - 3 < 0 ? 0 : EndPos - 3, IsSecondLine ? 2 : 0);
-            Console.Write(ChipCountText);
-            Console.SetCursorPosition(EndPos - 4 < 0 ? 0 : EndPos - 4, IsSecondLine ? 2 : 0);
-            Console.Write("|");
+            if (c.FourthNumber < 2)
+            {
+                Console.SetCursorPosition(Pos, 0);
+                Console.Write("|");
+                Console.SetCursorPosition(Pos - 1 < 0 ? 0 : Pos - 1, 0);
+                if (c.Identity == CellIIdentity.Free) Console.Write("н");
+                if (c.Identity == CellIIdentity.White) Console.Write("б");
+                if (c.Identity == CellIIdentity.Black) Console.Write("ч");
+                Console.SetCursorPosition(Pos - 3 < 0 ? 0 : Pos - 3, 0);
+                Console.Write(ChipCountText);
+                Console.SetCursorPosition(Pos - 4 < 0 ? 0 : Pos - 4, 0);
+                Console.Write("|");
+            }
+            else
+            {
+                Console.SetCursorPosition(Pos, 2);
+                Console.Write("|");
+                Console.SetCursorPosition(Pos + 1, 2);
+                Console.Write(ChipCountText);
+                Console.SetCursorPosition(Pos + 3, 2);
+                if (c.Identity == CellIIdentity.Free) Console.Write("н");
+                if (c.Identity == CellIIdentity.White) Console.Write("б");
+                if (c.Identity == CellIIdentity.Black) Console.Write("ч");
+                Console.SetCursorPosition(Pos + 4, 2);
+                Console.Write("|");
+            }
         }
     }
 }
