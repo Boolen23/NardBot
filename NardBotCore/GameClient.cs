fa0828144c75881253c45ce9c421524709be4ce0
dis <d>
2021-08-04 14:46:10 +0300

    6

---
+++ NardBotCore/GameClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NardBotCore
+{
+    public class GameClient
+    {
+        public GameClient(Game g, Identity identity)
+        {
+            //MoveStarted += GameClient_MoveStarted;
+            if (identity == Identity.White)
+                game.WhiteMove += MoveStarted;
+            else
+                game.BlackMove += MoveStarted;
+
+            this.ClientIdentity = identity;
+        }
+        private Game game;
+        protected virtual void MoveStarted(object sender, MoveEventArgs e)
+        {
+        }
+
+        public Identity ClientIdentity { get; private set; }
+
+    }
+}
